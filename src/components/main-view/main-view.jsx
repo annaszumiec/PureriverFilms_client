f34d17433c22266f4f8324626bc89feb75ecbc4c
annamaria <szumiecam@gmail.com>
2023-02-24 15:36:23 +0100

    movie view error

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,48 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
 import { MovieCard} from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
+  const [ movies, setMovies] = useState ([]);
 
-  const [ movies, setMovies] = useState ([
-    {
-      id: 1,
-      title: "Gangs of New York",
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/a/ae/Gangs_of_New_York_Poster.jpg",
-      director: "Martin Charles Scorsese"
-    },
-    {
-      id: 2,
-      title: "The Irish Man",
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/8/80/The_Irishman_poster.jpg",
-      director: "Martin Charles Scorsese"
-    },
-    {
-      id: 3,
-      title: "Silence",
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/3/36/Silence_%282016_film%29.png",
-      director: "Martin Charles Scorsese"
-    },
-    {
-      id: 4,
-      title: "The Wolf of Wall Street",
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/d/d8/The_Wolf_of_Wall_Street_%282013%29.png",
-      director: "Martin Charles Scorsese"
-    },
-    {
-      id: 5,
-      title: "Finding Nemo",
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/2/29/Finding_Nemo.jpg",
-      director: "Andrew Stanton"
-    },
-  ]);
+  useEffect(() => {
+    fetch("https://pureriverfilms.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        // alert (JSON.stringify(data))
+        const moviesFromApi = data.map((doc) => {
+          return {
+            id: doc._id,
+            title: doc.Title,
+            image: doc.ImagePath,
+            director: doc.Director
+          };
+        });
+        setMovies(moviesFromApi);
+      });
+  }, []);
+
+
 const [selectedMovie, setSelectedMovie] = useState(null);
 if(selectedMovie) {
   return(
@@ -66,3 +48,5 @@ return(
   </div>
 );
 };
+
+// Here is where we define all the props constraints for the MovieCard
